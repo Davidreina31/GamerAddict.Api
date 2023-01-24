9526c4be3d51580bb73eec140444508ba8e55b3c
David Reina <dave.reina31@gmail.com>
2023-01-24 19:25:32 +0100

    create answerController

---
+++ GamerAddict.Api/Controllers/AnswerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamerAddict.BLL.Interfaces.Managers;
+using GamerAddict.Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GamerAddict.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AnswerController : Controller
+    {
+        private readonly IAnswerManager _manager;
+
+        public AnswerController(IAnswerManager answerManager)
+        {
+            _manager = answerManager;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Answer>>> Get()
+        {
+            return Ok(await _manager.GetAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Answer>> GetOne(int id)
+        {
+            return Ok(await _manager.GetById(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<ActionResult<Answer>> Post([FromBody]Answer answer)
+        {
+            return Ok(await _manager.Add(answer));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
--- GamerAddict.Api/Program.cs
+++ GamerAddict.Api/Program.cs
@@ -1,4 +1,9 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using GamerAddict.BLL.Interfaces.Managers;
+using GamerAddict.BLL.Manager;
+using GamerAddict.DAL.Interfaces.Repositories;
+using GamerAddict.DAL.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
+builder.Services.AddScoped<IAnswerManager, AnswerManager>();
 
 var app = builder.Build();
 
